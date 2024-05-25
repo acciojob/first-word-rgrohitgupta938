1c84256ec9b72fc81e9d54b816dbe45afd59a35c
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-05-25 16:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function firstWord(s) {
   // your code here
-	 const spaceIndex = str.indexOf(' ');
-    if (spaceIndex === -1) {
+	if (!str || str.indexOf(' ') === -1) {
         return str;
     }
-    return str.substring(0, spaceIndex);
+ 
+    return str.substring(0, str.indexOf(' '));
 }
 
 // Do not change the code below
 
-//const s = prompt("Enter String:");
+const s = prompt("Enter String:");
 alert(firstWord(s));
